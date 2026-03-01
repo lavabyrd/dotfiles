b092010526391d43595af58fc9d1c96981445285
Mark Preston <7063963+lavabyrd@users.noreply.github.com>
2026-03-01 13:10:21 +0000

    hide empty workspace indicators in sketchybar

--- config/.config/sketchybar/plugins/aerospace.sh
+++ config/.config/sketchybar/plugins/aerospace.sh
@@ -2,9 +2,12 @@
 . "$CONFIG_DIR/colors.sh"
 
 WS="${NAME#space.}"
+WIN_COUNT=$(aerospace list-windows --workspace "$WS" 2>/dev/null | wc -l | tr -d ' ')
 
 if [ "$WS" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set "$NAME" label.color="$ACCENT_COLOR"
+    sketchybar --set "$NAME" drawing=on label.color="$ACCENT_COLOR"
+elif [ "$WIN_COUNT" -gt 0 ]; then
+    sketchybar --set "$NAME" drawing=on label.color="$OVERLAY_COLOR"
 else
-    sketchybar --set "$NAME" label.color="$OVERLAY_COLOR"
+    sketchybar --set "$NAME" drawing=off
 fi
