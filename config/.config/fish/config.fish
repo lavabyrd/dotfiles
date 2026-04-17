cfb16f34fc060a410ed7140b4330529aaf750af9
Mark Preston <7063963+lavabyrd@users.noreply.github.com>
2026-04-17 08:11:03 +0100

    dotfiles update

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    if not set -q TMUX
+        exec tmux new-session -A -s main
+    end
 end
 
 # Environment Variables
